d12b9ca0cd4d9cf55aa23569fd14149e7b7fbbd0
iZabrodina <100682511+iZabrodina@users.noreply.github.com>
2022-03-03 19:27:04 +0300

    Update fun.h

--- include/fun.h
+++ include/fun.h
@@ -1,7 +1,3 @@
 // Copyright 2022 UNN-IASR
-#ifndef INCLUDE_FUN_H_
-#define INCLUDE_FUN_H_
-#include <cstdint>
-int64_t power(int64_t x, uint16_t n);
-
-#endif  // INCLUDE_FUN_H_
+#pragma once
+int64_t power(int64_t, uint16_t);
