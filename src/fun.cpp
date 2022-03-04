4d8329b7c80b13d2350e91b6d9fda4bb9d156d97
iZabrodina <100682511+iZabrodina@users.noreply.github.com>
2022-03-04 23:35:31 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,13 +1,10 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
 
-int64_t power(int64_t x, uint16_t n) 
-{
+int64_t power(int64_t x, uint16_t n) {
     int64_t answer = x;
-    if ((x % 1 == 0) && (n % 1 == 0) && (n > 0))
-    {
-        for (uint16_t i = 0; i < n - 1; i++)
-        {
+    if ((x % 1 == 0) && (n % 1 == 0) && (n > 0)){
+        for (uint16_t i = 0; i < n - 1; i++){
             answer = answer * x;
         }
     }
